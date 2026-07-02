f6725995432d372512f5c82e70d2233687019870
Transmuter3621 <erynclairesy@gmail.com>
2026-07-02 18:50:49 +0800

    feat: Finished partially working input

--- src/mediavault/tui/Input.java
+++ src/mediavault/tui/Input.java
@@ -1,9 +1,16 @@
 package mediavault.tui;
+
+import mediavault.enums.Genre;
 import mediavault.enums.MediaType;
 import mediavault.enums.Status;
+
 import mediavault.models.Anime;
+import mediavault.models.Novel;
+import mediavault.models.VideoGame;
 import mediavault.models.MediaEntry;
 import mediavault.models.MediaVault;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Input
@@ -63,6 +70,64 @@ public class Input
         System.out.print("Synopsis: ");
         String synopsis = scanner.nextLine();
 
+        ArrayList<Genre> genreList;
+        System.out.println("Genres:");
+        System.out.println("[1] ACTION");
+        System.out.println("[2] ADVENTURE");
+        System.out.println("[3] COMEDY");
+        System.out.println("[4] CRIME");
+        System.out.println("[5] DOCUMENTARY");
+        System.out.println("[6] DRAMA");
+        System.out.println("[7] FANTASY");
+        System.out.println("[8] HISTORICAL_FICTION");
+        System.out.println("[9] HORROR");
+        System.out.println("[10] MUSIC");
+        System.out.println("[11] MYSTERY");
+        System.out.println("[12] PSYCHOLOGICAL");
+        System.out.println("[13] ROMANCE");
+        System.out.println("[14] SCIENCE_FICTION");
+        System.out.println("[15] SPORTS");
+        System.out.println("[16] THRILLER");
+        System.out.println("Choose genres based on their numbers: ");
+        System.out.println("Put spaces between each number (e.g. 1 2 3 4)");
+        do {
+            int genre = scanner.nextInt();
+            if(genre == 1)
+                genreList.add(Genre.ACTION);
+            else if(genre == 2)
+                genreList.add(Genre.ADVENTURE);
+            else if(genre == 3)
+                genreList.add(Genre.COMEDY);
+            else if(genre == 4)
+                genreList.add(Genre.CRIME);
+            else if(genre == 5)
+                genreList.add(Genre.DOCUMENTARY);
+            else if(genre == 6)
+                genreList.add(Genre.DRAMA);
+            else if(genre == 7)
+                genreList.add(Genre.FANTASY);
+            else if(genre == 8)
+                genreList.add(Genre.HISTORICAL_FICTION);
+            else if(genre == 9)
+                genreList.add(Genre.HORROR);
+            else if(genre == 10)
+                genreList.add(Genre.MUSIC);
+            else if(genre == 11)
+                genreList.add(Genre.MYSTERY);
+            else if(genre == 12)
+                genreList.add(Genre.PSYCHOLOGICAL);
+            else if(genre == 13)
+                genreList.add(Genre.ROMANCE);
+            else if(genre == 14)
+                genreList.add(Genre.SCIENCE_FICTION);
+            else if(genre == 15)
+                genreList.add(Genre.SPORTS);
+            else if(genre == 16)
+                genreList.add(Genre.THRILLER);
+            else
+                System.out.println(genre + " is invalid.");
+        } while (scanner.hasNextInt());
+
         System.out.println("Status:");
         System.out.println("[P] - Planned");
         System.out.println("[I] - In-progress");
@@ -76,13 +141,40 @@ public class Input
         }
 
         if(entryType == "A")
-            vault.addEntry(Anime(release, title, synopsis, genres, alternative, studio, status));
+        {
+            Anime anime = new Anime(release, title, synopsis, genreList, alternative, studio, null);
+            if(status == "P")
+                anime.setStatus(Status.PLANNED);
+            else if(status == "I")
+                anime.setStatus(Status.IN_PROGRESS);
+            else if(status == "C")
+                anime.setStatus(Status.COMPLETED);
+            vault.addEntry(anime);
+        }
 
         else if(entryType == "N")
-            vault.addEntry(Novel(release, title, synopsis, genres, publisher, author, status, chapters));
+        {
+            Novel novel = new Novel(release, title, synopsis, genreList, publisher, author, null, chapters);
+            if(status == "P")
+                novel.setStatus(Status.PLANNED);
+            else if(status == "I")
+                novel.setStatus(Status.IN_PROGRESS);
+            else if(status == "C")
+                novel.setStatus(Status.COMPLETED);
+            vault.addEntry(novel);
+        }
 
         else if(entryType == "V")
-            vault.addEntry(VideoGame(release, title, synopsis, genres, publisher, studio, status));
+        {
+            VideoGame videoGame = new VideoGame(release, title, synopsis, genreList, publisher, studio, null);
+            if(status == "P")
+                videoGame.setStatus(Status.PLANNED);
+            else if(status == "I")
+                videoGame.setStatus(Status.IN_PROGRESS);
+            else if(status == "C")
+                videoGame.setStatus(Status.COMPLETED);
+            vault.addEntry(videoGame);
+        }
     }
 
     public void promptUpdate (MediaVault vault)
