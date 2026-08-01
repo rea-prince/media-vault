832b7a9b22130497a7d99aa55823f475713f7a59
Transmuter3621 <erynclairesy@gmail.com>
2026-08-01 22:21:32 +0800

    feat: Add ascending and descending to sort menu and initialize SortController file

---
+++ src/mediavault/controllers/SortController.java
@@ -0,0 +1,50 @@
+package mediavault.controllers;
+
+import mediavault.models.MediaEntry;
+import mediavault.models.MediaVault;
+
+import java.util.ArrayList;
+
+import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class SortController 
+{
+    public void ascendTitleSort (ArrayList<MediaEntry> entries)
+    {
+        for (int i = 0; i < entries.size(); i++)
+        {
+
+        }
+    }
+
+    public void descendTitleSort (ArrayList<MediaEntry> entries)
+    {
+        for (int i = 0; i < entries.size(); i++)
+        {
+            
+        }
+    }
+
+    public void ascendYearSort (ArrayList<MediaEntry> entries)
+    {
+        for (int i = 0; i < entries.size(); i++)
+        {
+
+        }
+    }
+
+    public void descendYearSort (ArrayList<MediaEntry> entries)
+    {
+        for (int i = 0; i < entries.size(); i++)
+        {
+            
+        }
+    }
+}
