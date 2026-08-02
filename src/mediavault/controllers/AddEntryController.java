5479a877bae0bbd808736bcd5f9d4df7ec2186b6
Transmuter3621 <erynclairesy@gmail.com>
2026-08-02 11:52:31 +0800

    feat: Complete base add entry prompt and initialize variables in controller

--- src/mediavault/controllers/AddEntryController.java
+++ src/mediavault/controllers/AddEntryController.java
@@ -2,6 +2,9 @@ package mediavault.controllers;
 
 import mediavault.models.MediaEntry;
 import javafx.fxml.FXML;
+import mediavault.enums.MediaType;
+import mediavault.enums.Genre;
+import mediavault.enums.Status;
 import mediavault.models.Anime;
 import mediavault.models.Novel;
 import mediavault.models.VideoGame;
@@ -11,30 +14,54 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AddEntryController 
 {
-    @FXML private Label entryType;
-    @FXML private Label entryDetails;
+    @FXML private TextField titleField;
+	@FXML private TextField releaseYearField;
+	@FXML private TextArea synopsisArea;
+    @FXML private ChoiceBox<MediaType> entryType;
+    @FXML private ChoiceBox<Status> entryStatus;
+    @FXML private ChoiceBox<Genre> entryGenre;
+    @FXML private TextField altTitleField;
+	@FXML private TextField studioField;
+    @FXML private TextField authorField;
+	@FXML private TextField publisherField;
+    @FXML private TextField chaptersField;
+
+    @FXML
+	public void initialize() 
+    {
+		entryType.getItems().setAll(MediaType.values());
+		entryStatus.getItems().setAll(Status.values());
+		entryGenre.getItems().setAll(Genre.values());
+	}
+
+    public void addEntryDetails (MediaEntry entry)
+    {
+        String title = titleField.getText().trim();
+		String yearText = releaseYearField.getText().trim();
+		String synopsis = synopsisArea.getText().trim();
+
+		if (title.isEmpty() || synopsis.isEmpty() || yearText.isEmpty())
+			return;
+    }
 
     public void typeSpecDisplay (MediaEntry entry)
     {
         if (entry instanceof Anime anime) 
         {
-            entryType.setText("Anime");
-            entryDetails.setText(anime.getStudio());
+            
         }
         else if (entry instanceof Novel novel) 
         {
-            entryType.setText("Novel");
-            entryDetails.setText(novel.getAuthor());
-
+            
         }
         else if (entry instanceof VideoGame videoGame)
         {
-            entryType.setText("Video Game");
-            entryDetails.setText(videoGame.getStudio());
-
+            
         }
     }
 }
